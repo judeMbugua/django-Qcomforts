09dbcc71a165edbcd64b1becdbe036ba858e0921
JudeMbugua <judewanyagi@gmail.com>
2024-03-04 17:55:17 +0300

    Update build_files.sh
    updated .sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-pip install wheel setuptools pip --upgrade
-pip install wheel setuptools pip --upgrade
+pip install wheel setuptools   
+pip install wheel setuptools
 pip freeze > requirements.txt
 python manage.py collectstatic
